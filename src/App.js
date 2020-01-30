e0ac2fb693b1aa4f474420f2467f78d791e65e10
MatthewBrophy <m.broph@gmail.com>
2020-01-30 15:58:09 -0800

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div className="app">App!</div>;
+  }
+}
+
+export default App;
